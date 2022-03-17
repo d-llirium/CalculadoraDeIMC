da6e9fe33c7bdef6ebbeaf769c116e83a14d3a6e
d-llirium <plimacarlos@gmail.com>
2022-03-17 17:02:56 -0300

    adicionar funções para os sliders

--- Calculadora de IMC/Controllers/CalculationViewController.swift
+++ Calculadora de IMC/Controllers/CalculationViewController.swift
@@ -21,7 +21,16 @@ class CalculationViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func calculateAndGoToResult(_ sender: UIButton) {
+    @IBAction func heightValueChanged(_ sender: UISlider) {
+        let formatted = String(format: "%.2fm", heightSlider.value)
+        heightLabel.text = formatted
+    }
+    @IBAction func weightValueChanged(_ sender: UISlider) {
+        let formatted = String(format: "%.1fkg", weightSlider.value)
+        weightLabel.text = formatted
+    }
+    @IBAction func goToResult(_ sender: UIButton) {
+        
     }
     
 }
